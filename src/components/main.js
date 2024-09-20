2370a8b8f89af56ecc97a07e3756c8d2f5ec7769
Chengkai Lin <73312449+MKglory@users.noreply.github.com>
2024-09-19 23:04:04 -0400

    Project initlization

---
+++ src/components/main.js
@@ -0,0 +1,16 @@
+// src/BootstrapComponent.js
+
+import React from 'react';
+
+function BootstrapComponent() {
+  return (
+    <div className="container">
+        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+            Launch Modal
+        </button>
+        sfds
+    </div>
+  );
+}
+
+export default BootstrapComponent;
