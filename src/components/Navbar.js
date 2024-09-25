b8c3b7d1688c6e9ff050747a9d5b0c7d19935670
TheAKProgrammer <107050884+TheAKProgrammer@users.noreply.github.com>
2024-09-25 01:54:31 -0400

    Included more buttons on the nav bar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,24 +1,58 @@
 import React from 'react';
 
-function Navbar({ onStateChange }) {
+function Navbar({ onStateChange, onTabChange }) {
   const handleSelect = (event) => {
     onStateChange(event.target.value);
   };
 
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark">
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <div className="container-fluid">
-        <a className="navbar-brand" href="#">project</a>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" 
-          aria-controls="navbarNav" aria-expanded="false" aria-label="切换导航">
-          <span className="navbar-toggler-icon">test</span>
+        <a className="navbar-brand" href="#">Project</a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav ms-auto">
+            {/* Navigation List Items */}
+            <li className="nav-item">
+              <a className="nav-link" href="#home">Home</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#about">About</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#services">Services</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#contact">Contact</a>
+            </li>
+            
+            {/* Graph and Map Tabs */}
+            <li className="nav-item">
+              <button className="nav-link btn btn-link" onClick={() => onTabChange('map')}>
+                Map
+              </button>
+            </li>
+            <li className="nav-item">
+              <button className="nav-link btn btn-link" onClick={() => onTabChange('graph')}>
+                Graph
+              </button>
+            </li>
+
+            {/* Dropdown for State Selection */}
             <li className="nav-item dropdown">
               <select className="form-select" onChange={handleSelect} defaultValue="NY">
                 <option value="NY">New York (NY)</option>
-                <option value="AR">Arkansas (Arkansas)</option>
+                <option value="AR">Arkansas (AR)</option>
               </select>
             </li>
           </ul>
