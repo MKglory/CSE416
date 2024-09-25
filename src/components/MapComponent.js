49094432c0a92e8ee8af0b4b3d387facad9f552d
JiemingChen0 <83471007+JiemingChen0@users.noreply.github.com>
2024-09-25 00:19:13 -0400

    Add files via upload

--- src/components/MapComponent.js
+++ src/components/MapComponent.js
@@ -1,21 +1,33 @@
 import React, { useEffect, useState } from 'react';
-import { MapContainer, TileLayer, GeoJSON } from 'react-leaflet';
+import { MapContainer, TileLayer, GeoJSON, useMap  } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import nyData from '../data/ny_districts.json';
-//import arData from '../data/ar_districts.geojson';
+import arData from '../data/ar_Boundaries_ELECTION_PRECINCTS_polygon.json';
 import L from 'leaflet';
 //import './MapComponent.css';
+import { memo } from 'react';
+
+const nyCenter = [42.965, -76.0167];
+const arCenter = [34.7465, -92.2896];
 
 function MapComponent({ selectedState }) {
   const [geoData, setGeoData] = useState(null);
 
+  function ChangeMapView({ center }) {
+    const map = useMap();
+    useEffect(() => {
+      map.flyTo(center, 7);
+    }, [center, map]);
+    return null;
+  }
+
   useEffect(() => {
     if (selectedState === 'NY') {
       setGeoData(nyData);
-    } 
-    // else if (selectedState === 'AR') {
-    //   setGeoData(arData);
-    // }
+    }
+    else if (selectedState === 'AR') {
+      setGeoData(arData);
+    }
   }, [selectedState]);
 
   const onEachFeature = (feature, layer) => {
@@ -42,21 +54,38 @@ function MapComponent({ selectedState }) {
 
   const getColor = (d) => {
     return d === 'Republican' ? '#ff0000' :
-           d === 'Democratic' ? '#0000ff' :
-           '#00ff00';
+      d === 'Democratic' ? '#0000ff' :
+        '#00ff00';
   };
 
+  const usBounds = [
+    [24.396308, -124.848974], // Southwest
+    [49.384358, -66.885444]   // Northeast
+  ];
+
+  const mapCenter = selectedState === 'NY' ? nyCenter : arCenter;
+
   return (
-    <MapContainer center={[37.8, -96]} zoom={4} style={{ height: '600px', width: '100%' }}>
-      <TileLayer
-        attribution='&copy; <a href="https://osm.org/copyright">OpenStreetMap</a>'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      {geoData && (
-        <GeoJSON data={geoData} style={style} onEachFeature={onEachFeature} />
-      )}
-    </MapContainer>
+    <div style={{ paddingLeft: '30px', paddingRight: '30px' }} >
+      <MapContainer 
+        center={mapCenter}
+        bounds={usBounds}
+        maxBounds={usBounds}
+        maxZoom={12}
+        minZoom={5}
+        scrollWheelZoom={true}
+      >
+        <ChangeMapView center={mapCenter} />
+        <TileLayer
+          attribution='&copy; <a href="https://osm.org/copyright">OpenStreetMap</a>'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        {geoData && (
+          <GeoJSON data={geoData} style={style} onEachFeature={onEachFeature} />
+        )}
+      </MapContainer>
+    </div>
   );
 }
 
-export default MapComponent;
+export default memo(MapComponent);
